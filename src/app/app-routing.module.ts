39e15b0b7ad9e6ccd9c7787852c381dc11d8e80b
Sindhu M <Sindhu.M@godigit.com>
2024-08-13 14:51:18 +0530

    Login and Registration is done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
+
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  {
+    path:'login',
+    component:LoginComponent
+  },
+  {
+    path:'',
+    component:RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+
